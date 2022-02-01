7bd04ca3d98aba970fa275333e5017bfa2831107
nheckman-29 <nheckman29@gmail.com>
2022-02-01 10:22:01 -0500

    Added CSV

--- warranty.py
+++ warranty.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+#HP Warranty Retrieval - Nathan Heckman
+#Works off of 'All Systems' report from SCCM
+
+import pandas as p
+import os as os
+
+#Change directory to where the CSV file is or navigate in shell where Python is running
+#os.chdir(PATH)
+
+file = p.read_csv('all_systems.csv')
+
